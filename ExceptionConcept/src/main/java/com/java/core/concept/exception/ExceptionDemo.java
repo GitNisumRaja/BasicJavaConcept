701fdd6e8262c23fe7ca3b2e56fd1855c1b9a5be
Nisum <nisum@Nisums-AMacBook.local>
2019-07-13 13:47:57 +0530

    adding project structure with simple class example

---
+++ ExceptionConcept/src/main/java/com/java/core/concept/exception/ExceptionDemo.java
@@ -0,0 +1,14 @@
+package com.java.core.concept.exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class ExceptionDemo 
+{
+    public static void main( String[] args )
+    {
+        int a = 5, b = 0;
+        System.out.println(a/b);
+    }
+}
---
+++ StringConcept/src/main/java/com/java/core/concept/string/StringInput.java
@@ -0,0 +1,18 @@
+package com.java.core.concept.string;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringInput 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);
+        
+    }
+}
