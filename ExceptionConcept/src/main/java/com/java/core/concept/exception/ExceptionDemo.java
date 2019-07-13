76ff296aec5120250ca92d7e9d0f8d25b28b02dd
Afroz <nisum@192.168.1.9>
2019-07-13 17:39:28 +0530

    added try and catch block

--- ExceptionConcept/src/main/java/com/java/core/concept/exception/ExceptionDemo.java
+++ ExceptionConcept/src/main/java/com/java/core/concept/exception/ExceptionDemo.java
@@ -9,6 +9,10 @@ public class ExceptionDemo
     public static void main( String[] args )
     {
         int a = 5, b = 0;
+        try {
         System.out.println(a/b);
+        }catch(ArithmeticException ae) {
+        	System.out.println("catch block::"+ae.getMessage());
+        }
     }
 }
